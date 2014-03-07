aff1ffbbac8021c40e773d92ad7d79930bfd847b
dron4eeek <developer@gmail.com>
2014-03-08 00:42:10 +0200

    added parse com upload photos

--- Sample Parse.com Photo Video App/ViewControllers/PCPhotoUploadViewController.m
+++ Sample Parse.com Photo Video App/ViewControllers/PCPhotoUploadViewController.m
@@ -8,6 +8,8 @@
 
 #import "PCPhotoUploadViewController.h"
 #import <CTAssetsPickerController.h>
+#import "MBProgressHUD.h"
+#include <stdlib.h>
 
 @interface PCPhotoUploadViewController () <CTAssetsPickerControllerDelegate, UINavigationControllerDelegate>
 
@@ -55,8 +57,73 @@
 		if (iref)
     {
 			UIImage * image = [UIImage imageWithCGImage:iref];
+       NSData *imageData = UIImageJPEGRepresentation(image, 4.0);
+      [self uploadImage:imageData];
     }
 	}
+}
+
+- (void)uploadImage:(NSData*)imageData
+{
+  PFFile *imageFile = [PFFile fileWithName:@"Image.jpg" data:imageData];
+  
+  //HUD creation here (see example for code)
+  
+  // Save PFFile
+  [imageFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+    if (!error) {
+      // Hide old HUD, show completed HUD (see example for code)
+      
+      // Create a PFObject around a PFFile and associate it with the current user
+      PFObject *userPhoto = [PFObject objectWithClassName:@"Photos"];
+      [userPhoto setObject:imageFile forKey:@"photo"];
+      
+      // Set the access control list to current user for security purposes
+      userPhoto.ACL = [PFACL ACLWithUser:[PFUser currentUser]];
+      
+      PFUser *user = [PFUser currentUser];
+      [userPhoto setObject:user forKey:@"user"];
+      
+      [userPhoto saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+//          [self refresh:nil];
+        }
+        else{
+          // Log details of the failure
+          NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+      }];
+    }
+    else{
+//      [HUD hide:YES];
+      // Log details of the failure
+      NSLog(@"Error: %@ %@", error, [error userInfo]);
+    }
+  } progressBlock:^(int percentDone) {
+    // Update your progress spinner here. percentDone will be between 0 and 100.
+//    HUD.progress = (float)percentDone/100;
+  }];
+}
+
+- (void)downloadAllImages
+{
+  PFQuery *query = [PFQuery queryWithClassName:@"Photos"];
+  PFUser *user = [PFUser currentUser];
+  [query whereKey:@"user" equalTo:user];
+  [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+  {
+    // If there are photos, we start extracting the data
+    // Save a list of object IDs while extracting this data
+    
+    NSMutableArray *newObjectIDArray = [NSMutableArray array];
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    if (objects.count > 0) {
+      for (PFObject *eachObject in objects)
+      {
+        [newObjectIDArray addObject:[eachObject objectId]];
+      }
+    }
+  }];
 }
 
 - (NSArray *)indexPathOfNewlyAddedAssets:(NSArray *)assets
