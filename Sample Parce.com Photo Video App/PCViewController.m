ccdcfd780ec0254a2a4d46f07c841f1872756733
dron4eeek <developer@gmail.com>
2014-03-07 22:35:55 +0200

    added Xcode Project

---
+++ Sample Parce.com Photo Video App/PCViewController.m
@@ -0,0 +1,29 @@
+//
+//  PCViewController.m
+//  Sample Parce.com Photo Video App
+//
+//  Created by Developer on 3/7/14.
+//  Copyright (c) 2014 a.molchanovskiy. All rights reserved.
+//
+
+#import "PCViewController.h"
+
+@interface PCViewController ()
+
+@end
+
+@implementation PCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Sample Parce.com Photo Video App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample Parce.com Photo Video App
+//
+//  Created by Developer on 3/7/14.
+//  Copyright (c) 2014 a.molchanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+  }
+}
