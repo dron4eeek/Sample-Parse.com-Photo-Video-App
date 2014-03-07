bf839b12c3e7f7cb8516c50784068d30df4137fc
dron4eeek <developer@gmail.com>
2014-03-07 22:42:51 +0200

    added Xcode Project

---
+++ Sample Parse.com Photo Video AppTests/Sample_Parse_com_Photo_Video_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  Sample_Parse_com_Photo_Video_AppTests.m
+//  Sample Parse.com Photo Video AppTests
+//
+//  Created by Developer on 3/7/14.
+//  Copyright (c) 2014 a.molchanovskiy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sample_Parse_com_Photo_Video_AppTests : XCTestCase
+
+@end
+
+@implementation Sample_Parse_com_Photo_Video_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
